3a3b190af4006101138ff0597d22248b10a19e28
unknown <iuchi@PCS11019.ad.tgl.co.jp>
2012-05-15 19:54:14 +0900

    new

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'http://rubygems.org'
+
+gem 'sinatra', '1.3.2'
+gem 'haml'
---
+++ app.rb
@@ -0,0 +1,50 @@
+#coding:utf-8
+
+require 'rubygems'
+require 'sinatra'
+require 'haml'
+
+@@messages = []
+
+get '/' do
+  @name = ""
+  @error_msg = nil
+  set :public, File.dirname(__FILE__) + '/public'
+  set :haml, :format => :html5
+
+  haml :index
+end
+
+put '/room' do
+  set_message
+  name = request[:name]
+
+  if name.empty?
+    @error_msg = "ログイン名を入力してください。"
+    haml :index
+  else
+    @name = name
+    haml :room
+  end
+end
+
+post '/room' do
+  set_message
+
+  @name = get_name
+  haml :room
+end
+
+private
+
+def get_name
+  name = request[:name].nil? ? "" : request[:name]
+end
+
+def set_message
+  name = get_name
+  unless request[:message].nil? || request[:message].empty?
+    @@messages <<  [name, request[:message]]
+  end
+
+end
